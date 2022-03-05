314a269e15b70fbae9ddcef3915e30cbbd187480
DmitryShved <rennands@outlook.com>
2022-03-06 00:22:41 +0300

    init project

---
+++ src/main/java/com/outlook/rennands/restaurantVotingSystem/RestaurantVotingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.outlook.rennands.restaurantVotingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantVotingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantVotingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/outlook/rennands/restaurantVotingSystem/RestaurantVotingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.outlook.rennands.restaurantVotingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantVotingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
